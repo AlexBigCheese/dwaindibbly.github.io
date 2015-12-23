5cd1b8c5134c952124302fa9cbb393c2cfa72049
ajclarke903 <ajclarke903@gmail.com>
2015-12-23 09:36:07 -0500

    Wow, cards and more buttons! and link to memes *new*

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $("a.button").addClass("buttonru");
+$("a.buttonf").addClass("buttonfu");
 $("a.button").mouseenter(function () {
   $(this).removeClass("buttonru");
   $(this).addClass("buttonrf");
@@ -7,3 +8,25 @@ $("a.button").mouseleave(function () {
   $(this).removeClass("buttonrf");
   $(this).addClass("buttonru");
 });
+$("a.button").mousedown(function () {
+  $(this).removeClass("buttonrf");
+  $(this).addClass("buttonrd");
+})
+$("a.button").mouseup(function () {
+  
+})
+$("a.buttonf").mouseenter(function () {
+  $(this).removeClass("buttonfu");
+  $(this).addClass("buttonff");
+})
+$("a.buttonf").mouseleave(function () {
+  $(this).removeClass("buttonff");
+  $(this).addClass("buttonfu");
+})
+$("a.buttonf").mousedown(function () {
+  $(this).removeClass("buttonff");
+  $(this).addClass("buttonfd");
+})
+$("a.buttonf").mouseup(function () {
+  $(this).removeClass("buttonfd").addClass("buttonff")
+})
